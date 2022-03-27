9bd91c2edc7d1e7ea6fb5e1cd59e0e4163c56cca
Nicolo Danna <ndanna128@gmail.com>
2022-03-27 10:10:39 -0400

    error

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+//import the Sequalize constructor from the library
+const Sequalize = require('sequelize');
+
+require('dotenv').config();
+
+
+//Create connection to our databse, pass in MySql information for username and password
+const sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
