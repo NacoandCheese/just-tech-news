e89c4cedf2c6be2f4405c9e772b69ee6562350f9
Nicolo Danna <ndanna128@gmail.com>
2022-03-29 17:20:12 -0400

    added comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequalize = require('../../config/connection');
 const { sequelize } = require('../../models/User');
 
@@ -16,6 +16,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
